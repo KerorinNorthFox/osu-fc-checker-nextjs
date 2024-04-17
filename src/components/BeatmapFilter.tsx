7a5cb02af2498480ce6a8a5ef97e5e9f21ae179d
kerorinnf <kitakitsune.gingisune.8220A@gmail.com>
2024-04-17 14:17:05 +0900

    BeatmapFilterのviewを作成

---
+++ src/components/BeatmapFilter.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { Card } from "@yamada-ui/react";
+
+// ==============================
+// 譜面をフィルターするメニュー
+// ==============================
+
+const BeatmapFilter = () => {
+  return (
+    <>
+      <Card
+        padding={5}
+        margin={5}
+        backgroundColor="gray.100">
+        TODO: BeatmapFilter
+      </Card>
+    </>
+  );
+};
+
+export default BeatmapFilter;
